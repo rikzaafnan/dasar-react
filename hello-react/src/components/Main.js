d2f3bc95b63853ddd7cb9ebe4de8dd152a27ff31
Rick <rikzaafnan@gmail.com>
2019-05-17 09:52:29 +0700

    lanjutan belajar

--- hello-react/src/components/Main.js
+++ hello-react/src/components/Main.js
@@ -5,21 +5,45 @@ class Main extends Component{
         super(props);
         this.state={
             title : "Menu Makanan",
-            judul: "Ini percobaan Judul"
+            judul: "Ini percobaan Judul",
+            title2 : "Menu Minuman",
+            jenisminuman : "minuman dingin",
+            jenisminuman2 : "minuman panas"
         }
         this.rubahData = this.rubahData.bind(this)
+        this.cobaAja = this.cobaAja.bind(this)
+    }
+
+    // rubahData(){
+    //     this.state({title:"Pilih Makanan :"})
+    // }
+
+    cobaAja(){
+        this.setState((state, props)=>{
+            return {
+                jenisminuman : state.jenisminuman2
+            }
+        })
     }
 
 
     rubahData(){
-        this.setState({title:"pilih makanan"})
+        this.setState((state, props)=>{
+            return {
+                        title: state.title2,
+                        title2: state.title
+                    }
+        })
     }
     render(){
         return (    
             <div>
                 <h3>{this.state.title}</h3>
+                <h2>{this.state.title2}</h2>
                 <h5>{this.state.judul}</h5>
                 <button onClick={this.rubahData}>Rubah data</button>
+                <button onClick={this.cobaAja}>minuman nih</button>
+                <p>{this.state.jenisminuman}</p>
             </div> 
         )
     }
